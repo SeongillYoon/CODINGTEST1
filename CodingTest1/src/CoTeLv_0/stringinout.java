273974a05556b1140347c79d774f52497528a561
SeongillYoon <131629290+SeongillYoon@users.noreply.github.com>
2023-10-09 23:35:27 +0900

    문자열 출력하기

--- CodingTest1/src/CoTeLv_0/stringinout.java
+++ CodingTest1/src/CoTeLv_0/stringinout.java
@@ -1,5 +1,12 @@
 package CoTeLv_0;
 
-public class stringinout {
+import java.util.Scanner;
 
+public class stringinout {
+	// 코딩테스트 정답 Home
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.print(a);
+    }
 }
