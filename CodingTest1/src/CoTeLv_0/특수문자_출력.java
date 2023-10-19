e0c29bfb38197ed9de4903812b6151104fa808b6
SeongillYoon <131629290+SeongillYoon@users.noreply.github.com>
2023-10-20 01:40:34 +0900

    특수문자 출력하기

---
+++ CodingTest1/src/CoTeLv_0/특수문자_출력.java
@@ -0,0 +1,9 @@
+package CoTeLv_0;
+
+public class 특수문자_출력 {
+
+	public static void main(String[] args) {
+		System.out.print("!@#$%^&*(\\'\"<>?:;");
+		// \는 \\를 사용하고, '와 "사이에 \를 사용함으로써 분리시킨다.
+	}
+}
